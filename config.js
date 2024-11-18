b64b558e61a00fc14eff17293a914ab294de95a2
Hebimalu0000 <hebimalu0000@gmail.com>
2024-11-18 19:54:49 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC4guPinRRwZ3AlBdeiZMbn4XKq6_-d1Bo",
+  authDomain: "auth-test-556c5.firebaseapp.com",
+  projectId: "auth-test-556c5",
+  storageBucket: "auth-test-556c5.firebasestorage.app",
+  messagingSenderId: "274619285634",
+  appId: "1:274619285634:web:d07e6c6d136041e23b749b"
+};
